8c296b83cf862beb37f1abbc85d29bcbf79de32e
Andreina Palheta <palhetaandreina@gmail.com>
2024-11-12 16:02:17 -0300

    chore: removendo seed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UserService } from './user/user.service';
-import { VeiculoService } from './veiculo/veiculo.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,11 +19,11 @@ async function bootstrap() {
     new Logger().log(`Servidor rodando na porta: ${process.env.PORT ?? 3000}`);
   });
 
-  const userService = app.get(UserService);
-  await userService.seed();
+  // const userService = app.get(UserService);
+  // await userService.seed();
 
-  const veiculoService = app.get(VeiculoService);
-  await veiculoService.seed();
+  // const veiculoService = app.get(VeiculoService);
+  // await veiculoService.seed();
 }
 
 bootstrap();
